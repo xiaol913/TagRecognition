f3af3cf3f661a80a4338ef6426ad616bc23c7254
Shawn Gao <xxlangdou@gmail.com>
2016-10-24 18:47:00 -0500

    1st time

--- extractTag.py
+++ extractTag.py
@@ -7,24 +7,25 @@ def extractTag(name):
     x = cv2.Sobel(img, cv2.CV_16S, 2, 0)
     dst = cv2.convertScaleAbs(x, 0, 0.00390625)
     ret, temp = cv2.threshold(dst, 0, 255, cv2.THRESH_OTSU)
-    square = cv2.getStructuringElement(cv2.MORPH_RECT, (6, 2))
+    square = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
+    # cv2.imshow('1', temp)
     temp = cv2.dilate(temp, square, iterations=4)
     temp = cv2.erode(temp, square, iterations=4)
     temp = cv2.dilate(temp, square, iterations=3)
-    square = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 6))
+    square = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
     temp = cv2.erode(temp, square, iterations=1)
     temp = cv2.dilate(temp, square, iterations=3)
     # cv2.imshow('123', temp)
     contours, heirs = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for tours in contours:
         rc = cv2.boundingRect(tours)
-        if rc[2] / rc[3] >= 2 | rc[2] / rc[3] <= 4:
-            if 50 < rc[3] < 100:
+        if rc[2] / rc[3] >= 2:
+            if 50 < rc[3]:
                 cv2.rectangle(img, (rc[0], rc[1]), (rc[0] + rc[2], rc[1] + rc[3]), (255, 0, 255))
                 ball = img[rc[1]:(rc[1] + rc[3]), rc[0]:(rc[0] + rc[2])]
                 cv2.imwrite('tag'+ name, ball)
 
-    # cv2.imshow('123', img)
+    # cv2.imshow('1234', img)
     return
 
 def grayImg(name):
@@ -47,5 +48,5 @@ for i in range(256):
 extractTag('truecar.jpg')
 grayImg('truecar.jpg')
 #
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
