8d5a1aed29484d63cc25b187380fb2b6aad6e466
Shawn Gao <xxlangdou@gmail.com>
2016-10-24 23:42:44 -0500

    1st time

--- extractTag.py
+++ extractTag.py
@@ -1,6 +1,9 @@
 import cv2
 from PIL import Image
+import numpy as np
 import os
+import sys
+
 
 def extractTag(name):
     img = cv2.imread(name, 0)
@@ -15,26 +18,43 @@ def extractTag(name):
     square = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
     temp = cv2.erode(temp, square, iterations=1)
     temp = cv2.dilate(temp, square, iterations=3)
-    # cv2.imshow('123', temp)
+    # cv2.imshow('2', temp)
     contours, heirs = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.imshow('3', temp)
     for tours in contours:
         rc = cv2.boundingRect(tours)
         if rc[2] / rc[3] >= 2:
             if 50 < rc[3]:
                 cv2.rectangle(img, (rc[0], rc[1]), (rc[0] + rc[2], rc[1] + rc[3]), (255, 0, 255))
                 ball = img[rc[1]:(rc[1] + rc[3]), rc[0]:(rc[0] + rc[2])]
-                cv2.imwrite('tag'+ name, ball)
+                name = 'tag_' + name
+                cv2.imwrite(name, ball)
 
     # cv2.imshow('1234', img)
+    grayTag(name)
     return
 
-def grayImg(name):
-    img = Image.open('tag'+name)
+
+def grayTag(name):
+    img = Image.open(name)
     imgry = img.convert('L')
     out = imgry.point(table, '1')
-    out.save('brg'+ name)
-    os.remove('tag'+name)
+    os.remove(name)
+    out.save(name)
+    RemoveBorder(name)
     return
+
+
+def RemoveBorder(name):
+    img = cv2.imread(name, 0)
+    h, w = img.shape[:2]
+    tagROI = img[15:h - 15, 10:w - 10]
+    mask = np.zeros((h, w), np.uint8)
+    mask = cv2.bitwise_not(mask)
+    mask[15:h - 15, 10:w - 10] = tagROI
+    os.remove(name)
+    name = '#_' + name
+    cv2.imwrite(name, mask)
 
 
 threshold = 140
@@ -45,8 +65,5 @@ for i in range(256):
     else:
         table.append(1)
 
-extractTag('truecar.jpg')
-grayImg('truecar.jpg')
-#
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+if __name__ == "__main__":
+    extractTag(sys.argv[1])
--- opencvtest.py
+++ opencvtest.py
@@ -1,26 +1,25 @@
 import cv2
 import numpy as np
-from pytesseract import *
 from PIL import Image
-
-threshold = 140
-table = []
-for i in range(256):
-    if i < threshold:
-        table.append(0)
-    else:
-        table.append(1)
-
-img = cv2.imread('brgtruecar.jpg', 0)
+#
+# threshold = 140
+# table = []
+# for i in range(256):
+#     if i < threshold:
+#         table.append(0)
+#     else:
+#         table.append(1)
+#
+# img = cv2.imread('brgtruecar.jpg', 0)
 # img = cv2.bitwise_not(img)
-h, w = img.shape[:2]
-mask = np.zeros((h+5, w+5), np.uint8)
-mask = cv2.bitwise_not(mask)
-
-ball = img[30:46,30:250]
+# h, w = img.shape[:2]
+# mask = np.zeros((h+5, w+5), np.uint8)
+# mask = cv2.bitwise_not(mask)
+#
+# ball = img[30:46,30:250]
 # mask[10:46,10:162] = ball
 
-cv2.imshow('123', ball)
+# cv2.imshow('123', ball)
 
 
 # cv2.imwrite('newtag.jpg', mask)
@@ -36,5 +35,27 @@ cv2.imshow('123', ball)
 # print text
 # cv2.imshow('1', ball)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = cv2.imread('brgtruecar.jpg', 0)
+img = cv2.resize(img, (256, 64), interpolation=cv2.INTER_NEAREST)
+dst = cv2.convertScaleAbs(img, 0, 0.003)
+ret, temp = cv2.threshold(dst, 0, 255, cv2.THRESH_OTSU)
+square = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
+square = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
+temp = cv2.erode(temp, square, iterations=3)
+temp = cv2.dilate(temp, square, iterations=3)
+contours, heirs = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# cv2.imshow('temp', temp)
+for tours in contours:
+    rc = cv2.boundingRect(tours)
+    if rc[2] / rc[3] >= 2:
+        cv2.rectangle(img, (rc[0], rc[1]), (rc[0] + rc[2], rc[1] + rc[3]), (255, 0, 255))
+        ball = img[rc[1]:(rc[1] + rc[3]), rc[0]:(rc[0] + rc[2])]
+        ball = cv2.resize(ball, (256, 64), interpolation=cv2.INTER_LINEAR)
+        ball = cv2.bitwise_not(ball)
+        # cv2.imshow('123', ball)
+        cv2.imwrite('332211.jpg', ball)
+
+
+# cv2.imshow('origin', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
